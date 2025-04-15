aebb38655ad472e7840bf983d4a28946a30c6a0a
valentemnz <itsvalentemanzo@gmail.com>
2025-04-15 14:53:19 -0500

    Fix puerto para render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -20,4 +21,5 @@ def webhook():
     return "ok", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
